7f93b5115211dfcefb9243059ef25a4555f5f558
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2023-10-02 08:44:13 -0300

    refactor: removing unused comments

--- cypher.py
+++ cypher.py
@@ -15,6 +15,6 @@ def cypher(message, key):
         cyphertext += message[i]
       else:
         cypherCharacter = (ord(message[i]) + ord(keystream[i]) - 2 * ord('a')) % 26
-        cypherCharacter += ord('a')  # adicione 'a' para obter o caractere correspondente
-        cyphertext += chr(cypherCharacter)  # converta o valor para um caractere e adicione ao texto cifrado
+        cypherCharacter += ord('a')  # shift
+        cyphertext += chr(cypherCharacter)
   return cyphertext, keystream
