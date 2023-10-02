10c0b0a53e3a4c934bd3076a80039efb61a7a230
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2023-10-02 11:47:46 -0300

    fix: decypher and cypher functions

--- cypher.py
+++ cypher.py
@@ -10,11 +10,13 @@ def cypher(message, key):
     print("Erro ao gerar KeyStream, tente novamente")
   else:
     print(f"Keystream gerado: {keystream}")
+    normalCharacterIndex = 0
     for i in range(messageSize):
       if checkEspecialCharacter(message[i]):
         cyphertext += message[i]
       else:
-        cypherCharacter = (ord(message[i]) + ord(keystream[i]) - 2 * ord('a')) % 26
+        cypherCharacter = (ord(message[i]) + ord(keystream[normalCharacterIndex]) - 2 * ord('a')) % 26
         cypherCharacter += ord('a')  # shift
         cyphertext += chr(cypherCharacter)
+        normalCharacterIndex += 1
   return cyphertext, keystream
--- decypher.py
+++ decypher.py
@@ -3,12 +3,13 @@ from checkEspecialCharacter import checkEspecialCharacter
 def decypher(cyphertext, keystream):
   cyphertextSize = len(cyphertext)
   originalMessage = ""
-
+  normalCharacterIndex = 0
   for i in range(cyphertextSize):
     if checkEspecialCharacter(cyphertext[i]):
       originalMessage += cyphertext[i]
     else:
-      decypherCharacter = (ord(cyphertext[i]) - ord(keystream[i]) + 26) % 26
+      decypherCharacter = (ord(cyphertext[i]) - ord(keystream[normalCharacterIndex]) + 26) % 26
       decypherCharacter += ord('a') #* we have to add 'a' to shift our character after module operation
       originalMessage += (chr(decypherCharacter))
+      normalCharacterIndex += 1
   return originalMessage
